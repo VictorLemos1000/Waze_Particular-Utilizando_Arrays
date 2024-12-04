b48fccc693258550a9b15379a92d9cfc36aaa008
Williane Felix da Cruz Gonçalves <w.da.cruz.goncalves@avanade.com>
2024-12-03 21:59:11 -0300

    refactor: ultimos ajustes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+     // Cria a cidade inicial e configura a grade com o layout correspondente
     const cidade = new Cidade("Recife");
     const grade = new Grade(20, 45, cidade.obterLayout());
 
     grade.criarGrade();
 
+    // Configura botão de reset para limpar o trajeto e redefinir os pontos
     document.getElementById('reset-button').addEventListener('click', () => {
         grade.pontoInicial = null;
         grade.pontoFinal = null;
+
          // Limpa o trajeto pintado, voltando todas as células de ruas para a cor padrão
         const ruas = document.querySelectorAll('.ruas');
         ruas.forEach((celula) => {
@@ -19,17 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
     
+     // Botão para alternar entre cidades
     document.getElementById('mudaCidade').addEventListener('click', () => {
         const cidades = ["Recife", "Abreu e Lima", "Paulista", "Igarassu"];
 
         let novaCidade;
         do {
             novaCidade = cidades[Math.floor(Math.random() * cidades.length)];
-        } while (novaCidade === grade.layoutCidade);
+        } while (novaCidade === grade.nomeCidade);
     
         // Atualiza a cidade com o novo layout
         const cidadeAtualizada = new Cidade(novaCidade);
         grade.layoutCidade = cidadeAtualizada.obterLayout(); // Atualiza o layout
+        grade.nomeCidade = novaCidade; // Atualiza o nome da cidade no objeto grade
         grade.criarGrade(); // Recria a grade com o novo layout
     
         // Atualiza o título da cidade na interface
